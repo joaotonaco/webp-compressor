fa1d61b554e4ace842f9047c0394fe2b52035acd
João Gabriel Tonaco <blu3zin@gmail.com>
2024-02-22 11:12:54 -0300

    add dark mode to layout and start creating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import { GithubLink } from "@/components/github-link";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
-	return null;
+	return (
+		<main className="w-full h-screen p-24">
+			<Card className="w-fit mx-auto">
+				<CardHeader>
+					<CardTitle>WebP Compressor</CardTitle>
+					<CardDescription>
+						Opinionated WebP Compressor created by <GithubLink />.
+					</CardDescription>
+				</CardHeader>
+			</Card>
+		</main>
+	);
 }
---
+++ src/components/github-link.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export function GithubLink() {
+	return (
+		<Link
+			href="https://github.com/joaotonaco"
+			className="hover:underline underline-offset-4"
+		>
+			joaotonaco
+		</Link>
+	);
+}
